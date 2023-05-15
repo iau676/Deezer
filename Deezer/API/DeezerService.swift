527efc76b28c7a40f62793b98a28e30945369f4a
iau676 <ibrahimuysal676@gmail.com>
2023-05-15 15:05:34 +0300

    clean DeezerService

--- Deezer/API/DeezerService.swift
+++ Deezer/API/DeezerService.swift
@@ -9,60 +9,45 @@ import Foundation
 
 struct DeezerService {
     
-    static func fetchCategories(completion: @escaping([Category]) -> Void) {
-        if let url = URL(string: "https://api.deezer.com/genre") {
+    static func fetchData(urlStr: String, completion: @escaping(Data) -> Void) {
+        if let url = URL(string: urlStr) {
             let dataTask = URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) -> Void in
                 if error == nil {
                     if let data = data {
-                        let categoryResponse = try! JSONDecoder().decode(CategoryResponse.self, from: data)
-                        completion(categoryResponse.categories)
+                        completion(data)
                     }
                 }
             })
             dataTask.resume()
+        }
+    }
+    
+    static func fetchCategories(completion: @escaping([Category]) -> Void) {
+        fetchData(urlStr: "https://api.deezer.com/genre") { data in
+            let categoryResponse = try! JSONDecoder().decode(CategoryResponse.self, from: data)
+            completion(categoryResponse.categories)
         }
     }
     
     static func fetchArtists(withId id: Int, completion: @escaping([Artist]) -> Void) {
-        if let url = URL(string: "https://api.deezer.com/genre/\(id)/artists") {
-            let dataTask = URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) -> Void in
-                if error == nil {
-                    if let data = data {
-                        let artistResponse = try! JSONDecoder().decode(ArtistResponse.self, from: data)
-                        completion(artistResponse.artists)
-                    }
-                }
-            })
-            dataTask.resume()
+        fetchData(urlStr: "https://api.deezer.com/genre/\(id)/artists") { data in
+            let artistResponse = try! JSONDecoder().decode(ArtistResponse.self, from: data)
+            completion(artistResponse.artists)
         }
     }
     
     static func fetchAlbums(withArtistId id: Int, completion: @escaping([Album]) -> Void) {
-        if let url = URL(string: "https://api.deezer.com/artist/\(id)/albums") {
-            let dataTask = URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) -> Void in
-                if error == nil {
-                    if let data = data {
-                        let albumResponse = try! JSONDecoder().decode(AlbumResponse.self, from: data)
-                        completion(albumResponse.albums)
-                    }
-                }
-            })
-            dataTask.resume()
+        fetchData(urlStr: "https://api.deezer.com/artist/\(id)/albums") { data in
+            let albumResponse = try! JSONDecoder().decode(AlbumResponse.self, from: data)
+            completion(albumResponse.albums)
         }
     }
     
     static func fetchSongs(withAlbumId id: Int, completion: @escaping([Song]) -> Void) {
-        if let url = URL(string: "https://api.deezer.com/album/\(id)") {
-            let dataTask = URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) -> Void in
-                if error == nil {
-                    if let data = data {
-                        let albumDetail = try! JSONDecoder().decode(AlbumDetail.self, from: data)
-                        let songs = albumDetail.songResponse.songs
-                        completion(songs)
-                    }
-                }
-            })
-            dataTask.resume()
+        fetchData(urlStr: "https://api.deezer.com/album/\(id)") { data in
+            let albumDetail = try! JSONDecoder().decode(AlbumDetail.self, from: data)
+            let songs = albumDetail.songResponse.songs
+            completion(songs)
         }
     }
 }
